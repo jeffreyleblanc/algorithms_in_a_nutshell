0494eb9876c4990574a7fdd632813a47093f219e
Jeffrey LeBlanc <jeffreyleblanc@gmail.com>
2012-09-30 19:08:54 -0400

    Graphs: reorganization

--- graphs/src/algorithms/breadthFirstSearch.js
+++ graphs/src/algorithms/breadthFirstSearch.js
@@ -1,20 +1,4 @@
 
-function Queue () {
-    this.Q = new Array();
-    this.enqueue = function( obj ) {
-        this.Q.unshift( obj );
-    };
-    this.head = function() {
-        return this.Q[ this.Q.length-1 ];
-    };
-    this.pop = function() {
-        return this.Q.pop();
-    };
-    this.empty = function(){
-    	return this.Q.length == 0;
-    }
-}
-
 function breadthFirstSearch( graph, s ){
 
 	if( s == null ){ $.C('No source defined!');return; }
