5fa07da17081209bb955225539c43ed1767eb379
Jeffrey LeBlanc <jeffreyleblanc@gmail.com>
2012-10-27 19:51:40 -0400

    Geometry: KDTree build functions incorporated into the class itself

--- geometry/src/datastructures/KDTree.js
+++ geometry/src/datastructures/KDTree.js
@@ -5,81 +5,24 @@
 
 // http://en.wikipedia.org/wiki/K-d_tree
 
-
-function generateKDTree( points ){
-
-	if( points.length  == 0 ) return null;
-
-	// Median will be the root:
-	var maxD = 2 + 1; // hardcoded...
-	var tree = new KDTree( maxD );
-
-	// we need comparators for each dimension!
-	var comparators = [
-		function( p1, p2 ){
-			$.C('1: '+p1.a_id);
-			$.C('2: '+p2.a_id);
-			if( p1.x() > p2.x() ) return 1; // handle this better
-			else if( p1.x() < p2.x() ) return -1;
-			else{
-				if( p1.y() > p2.y() ) return 1;
-				else return -1;
-			}
-		},
-		function( p1, p2 ){
-			$.C('1: '+p1.a_id);
-			$.C('2: '+p2.a_id);
-			if( p1.y() > p2.y() ) return 1; //handle this better
-			else if( p1.y() < p2.y() ) return -1;
-			else{
-				if( p1.x() > p2.x() ) return 1;
-				else return -1;
-			}
-		}
-	];
-
-	tree.setRoot( generate(1, maxD, points, 0, points.length-1, comparators ) );
-	return tree;
-};
-
-function generate( d, maxD, points, left, right, comparators){
-
-	// Handle simple cases
-	if( right < left ) return null;
-	if( right == left ) return new KDNode().i( d, points[left] );
-
-	// Order the array[left,right] so the mth element will be the median
-	// and the elements prior to it will be <=, though they won't neccessarily
-	// be sorted; similarly, the elements afer will all be >=
-	var m = left+Math.floor((1+right-left)/2);
-	splitaboutKth( points, left, right, m, comparators[d-1] );
-
-	// Median point on this dimension becomes parent
-	var dm = new KDNode().i( d, points[m]);
-
-	// Update to the next dimension, or reset back to 1
-	d += 1;
-	if( d >= maxD ){ d = 1; }
-
-	// recursively compute the left and right sub-trees, which translate
-	// into 'below' and 'above' for n-dimensions
-	dm.setBelow(
-		generate( d, maxD, points, left, m-1, comparators )
-	);
-	dm.setAbove(
-		generate( d, maxD, points, m+1, right, comparators )
-	);
-	return dm;
-
-};
-
-
 ;(function(){var root=this;
 	
 	root.KDTree = aObj.$extend( {
 		//-- Class Vars ------------------------------------------------//
 			__classvars__ : {
-				aType : 'KDTree'
+				aType : 'KDTree',
+				comparators : [
+					function( p1, p2 ){
+						return ( p1.x()>p2.x() ? 1: (
+							p1.x()<p2.x() ? -1 :
+								( p1.y()>p1.y()? 1 : -1 )))
+					},
+					function( p1, p2 ){
+						return ( p1.y()>p2.y() ? 1: (
+							p1.y()<p2.y() ? -1 :
+								( p1.x()>p1.x()? 1 : -1 )))
+					}
+				]
 			},
 		
 		//-- Constructor & Destructor ------------------------------------------------//
@@ -91,6 +34,48 @@ function generate( d, maxD, points, left, right, comparators){
 				Q.maxDim = undefined;
 
 				Q.entries = [];
+			},
+
+
+			generate : function( points ){var Q=this;
+
+				if( points.length  == 0 ) return;
+
+				maxDim = 2 + 1; // hardcoded...
+
+				Q.setRoot( Q.generateNode(1, maxDim, points, 0, points.length-1, Q.$class.comparators ) );
+				return Q;
+			},
+
+			generateNode : function( d, maxD, points, left, right, comparators){var Q=this;
+
+				// Handle simple cases
+				if( right < left ) return null;
+				if( right == left ) return new KDNode().i( d, points[left] );
+
+				// Order the array[left,right] so the mth element will be the median
+				// and the elements prior to it will be <=, though they won't neccessarily
+				// be sorted; similarly, the elements afer will all be >=
+				var m = left+Math.floor((1+right-left)/2);
+				splitaboutKth( points, left, right, m, comparators[d-1] );
+
+				// Median point on this dimension becomes parent
+				var dm = new KDNode().i( d, points[m]);
+
+				// Update to the next dimension, or reset back to 1
+				d += 1;
+				if( d >= maxD ){ d = 1; }
+
+				// recursively compute the left and right sub-trees, which translate
+				// into 'below' and 'above' for n-dimensions
+				dm.setBelow(
+					Q.generateNode( d, maxD, points, left, m-1, comparators )
+				);
+				dm.setAbove(
+					Q.generateNode( d, maxD, points, m+1, right, comparators )
+				);
+				return dm;
+
 			},
 
 			dump : function(){var Q=this;
